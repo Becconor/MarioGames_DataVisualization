d99448f43935cff3c1eff581a3a0c24c9f69b9e7
zoiebackman <zoiebackman@icloud.com>
2026-05-04 12:38:17 +0200

    .

--- dataset/specificInfo.js
+++ dataset/specificInfo.js
@@ -2,7 +2,13 @@ const skills = ["balance", "precision", "speed", "strength", "speed"];
 
 const locations = ["desert", "mountain", "plain", "star", "underwater"];
 
-const disciplines = ["bobsleigh", "figure skating", "ski jumping", "speed skating", "snowboard cross"];
+const disciplines = [
+  "bobsleigh",
+  "figure skating",
+  "ski jumping",
+  "speed skating",
+  "snowboard cross",
+];
 
 const participants = ["44"];
 
