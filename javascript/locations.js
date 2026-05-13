7d8bcb744bebdf552c026d202d2af48601b2f6c8
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2026-05-13 15:07:27 +0200

    fixed the choice buttons

--- javascript/locations.js
+++ javascript/locations.js
@@ -40,9 +40,70 @@ let locationSections = [
 function locationsPage(location) {
     let world = locationInfo.find(p => p.title === location);
     let currentSection = locationSections[currentSectionIndex];
+    let sectionContent = "";
+
+    if (currentSection.title === "Game Days") {
+        sectionContent = `
+        <svg id="gameDaySVG"></svg>
+        `;
+    }
+
+    if (currentSection.title === "Best Players") {
+        sectionContent = `
+            <div class="L_ranking">
+
+                <div class="L_rankingFirstText">
+
+                    <h2 class="L_rankNumber">1</h2>
+
+                    <img
+                        class="L_rankPic"
+                        src="../images/players/amy.webp"
+                        alt=""
+                    >
+
+                    <h3 class="L_rankNames">
+                        Amy
+                    </h3>
+
+                </div>
+
+                <h3 class="L_rankPoints">
+                    93 pts
+                </h3>
+
+            </div>
+
+
+
+            <div class="L_ranking">
+
+                <div class="L_rankingFirstText">
+
+                    <h2 class="L_rankNumber">2</h2>
+
+                    <img
+                    class="L_rankPic"
+                    src="../images/players/mario.webp"
+                    alt=""
+                    >
+
+                    <h3 class="L_rankNames">
+                        Mario
+                    </h3>
+
+                </div>
+
+                <h3 class="L_rankPoints">
+                    88 pts
+                </h3>
+
+            </div>
+        `;
+    }
 
     mainDOM.innerHTML = `
-        <div id="L_world" class="L_worldSky">
+        <div id="L_world">
             <div class="back_track">p</div>
 
             <div id="L_worldText">
@@ -58,10 +119,11 @@ function locationsPage(location) {
             <img class="L_arrows" id="L_arrowsRight" src="../images/arrows/arrowRight.png" alt="">
         </div>
 
+        
         <div id="L_pickedLocationInfo">
             <p id="L_pickedInfoText">${currentSection.text}</p>
 
-            <div id="rankinglist"></div>
+            <div id="rankingList">${sectionContent}</div>
 
 
 
@@ -71,6 +133,24 @@ function locationsPage(location) {
 
         </div>
 
+    `;
+
+
+    // skapa rankning för spelarna som vann på location
+    let rankingList = document.getElementById("rankingList");
+    // for (let i = 0; i < participants.length; i++) {
+
+    // skapa divarna och elementen för rankningslistan av top spelare
+
+    // }
+
+    let worldPic = document.getElementById("L_world");
+    worldPic.style.backgroundImage = `
+        linear-gradient(
+            rgba(255,255,255,0.5),
+            rgba(255,255,255,0.5)
+        ),
+        url(${world.img})
     `;
 
     let leftArrow = document.getElementById("L_arrowsLeft");
@@ -98,55 +178,6 @@ function locationsPage(location) {
 
         locationsPage(location);
     });
-
-
-
-    // mainDOM.innerHTML = `
-    // <div id="L_infoChoice">
-    // <img class="L_arrows" id="L_arrowsLeft" src="../images/arrows/arrowLeft.png" alt="">
-    // <h2 id="locationInfoPick">Game Days</h2>
-    // <img class="L_arrows" id="L_arrowsRight" src="../images/arrows/arrowRight.png" alt="">
-    // </div>
-
-    //     <div id="L_pickedLocationInfo">
-    //     <p id="L_pickedInfoText">Explore the player ranking for this location. The list shows how all players performed
-    //         here, making it easy to identify the top performers and compare results within the selected location.</p>
-
-    //     <div class="L_ranking">
-    //         <div class="L_rankingFirstText">
-    //         <h2 class="L_rankNumber">1</h2>
-    //         <img class="L_rankPic" src="../images/players/amy.webp" alt="">
-    //         <h3 class="L_rankNames">Amy</h3>
-    //         </div>
-
-    //         <h3 class="L_rankPoints">93 pts</h3>
-    //     </div>
-
-    //     <div class="L_ranking">
-    //         <div class="L_rankingFirstText">
-    //         <h2 class="L_rankNumber">1</h2>
-    //         <img class="L_rankPic" src="../images/players/mario.webp" alt="">
-    //         <h3 class="L_rankNames">Amy</h3>
-    //         </div>
-
-    //         <h3 class="L_rankPoints">93 pts</h3>
-    //     </div>
-
-    //     <div class="L_ranking">
-    //         <div class="L_rankingFirstText">
-    //         <h2 class="L_rankNumber">1</h2>
-    //         <img class="L_rankPic" src="../images/players/babyLuigi.png" alt="">
-    //         <h3 class="L_rankNames">Amy</h3>
-    //         </div>
-
-    //         <h3 class="L_rankPoints">93 pts</h3>
-    //     </div>
-    //     </div>
-    // `;
-
-    // let arrowLeft = document.getElementById("L_arrowsLeft");
-    // let arrowRight = document.getElementById("L_arrowsRight");
-
 };
 
 
@@ -154,4 +185,4 @@ function worldInfoPick() {
 
 }
 
-locationsPage("Sky World");
+locationsPage("Desert World");
