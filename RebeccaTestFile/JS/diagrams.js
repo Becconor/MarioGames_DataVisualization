d43f34225b2d2fcef754bab289516197fea16267
zoiebackman <zoiebackman@icloud.com>
2026-05-20 11:09:19 +0200

    .

--- RebeccaTestFile/JS/diagrams.js
+++ RebeccaTestFile/JS/diagrams.js
@@ -30,11 +30,13 @@ function radarChart(sport) {
     .style("border", "1px solid black");
 
   //Skapa en grupp som ska placeras mitten, annars ligger det uppe i ett hörn
-  svg.append("g").attr("transform", `translate(${width / 2}, ${height / 2})`);
+  let middle = svg
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
   //skapa ringarna som är i cirkeln
   for (let level = 1; level <= levels; level++) {
-    svg
+    middle
       .append("circle")
       .attr("r", radiusScale(level))
       .attr("fill", "none")
@@ -48,7 +50,7 @@ function radarChart(sport) {
     const x = Math.cos(angle) * radius;
     const y = Math.sin(angle) * radius;
 
-    svg
+    middle
       .append("line")
       .attr("x1", 0)
       .attr("y1", 0)
@@ -62,9 +64,63 @@ function radarChart(sport) {
   skills.forEach((d, i) => {
     const angle = angleSlice * i - Math.PI / 2;
 
-    const x = Math.cos(angle) * radius;
-    const y = Math.sin(angle) * radius;
+    // radius + 15 så att det inte ligger exakt på yttersta linjen
+    const x = Math.cos(angle) * (radius + 15);
+    const y = Math.sin(angle) * (radius + 15);
+
+    middle
+      .append("text")
+      .attr("x", x)
+      .attr("y", y)
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "middle")
+      .style("font-weight", "bold")
+      .style("font-size", "15px")
+      .style("fill", "black")
+      .text("hej");
   });
+
+  //skapa radarformen
+  const radarLine = d3
+    .lineRadial()
+    .radius((d) => radiusScale(d.value))
+    .angle((d, i) => i * angleSlice)
+    .curve(d3.curveLinearClosed);
+
+  //skapa formen(färg)
+  middle
+    .append("path")
+    .datum(skills)
+    .attr("d", radarLine)
+    .attr("fill", "rgba(219, 52, 155, 0.45)")
+    .attr("stroke", "#b92989")
+    .attr("stroke-width", 3);
+
+  //lägg till punkter
+  data.forEach((d, i) => {
+    const angle = angleSlice * i - Math.PI / 2;
+
+    const x = Math.cos(angle) * radiusScale(d.value);
+    const y = Math.sin(angle) * radiusScale(d.value);
+
+    middle
+      .append("circle")
+      .attr("cx", x)
+      .attr("cy", y)
+      .attr("r", 4)
+      .attr("fill", "#2980b9");
+  });
+
+  for (let level = 0; level <= maxValue; level++) {
+    middle
+      .append("text")
+      .attr("x", radiusScale(level))
+      .attr("y", 15)
+      .attr("text-anchor", "middle")
+      .style("font-size", "13px")
+      .style("fill", "#555")
+      .text(level);
+  }
 }
 
 function scatterPlot() {
